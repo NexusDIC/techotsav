5ab37f1a8207c2fe81be751f6f551c88376642d6
amitingits <amituttamdas24@gmail.com>
2025-03-09 00:00:29 +0530

    updated

--- src/app/hackathon/page.tsx
+++ src/app/hackathon/page.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import Hero from "@/Components/Hero";
+import About from "@/Components/About";
+import Schedule from "@/Components/Schedule";
+import Speakers from "@/Components/Speaker";
+import Sponsors from "@/Components/Sponsors";
+import ComPats from "@/Components/ComPats";
+import Faq from "@/Components/Faq";
+import Navbar from "@/Components/Navbar";
 
-const page = () => {
-  return <div>hackathon</div>;
-};
-
-export default page;
+export default function Hackotsav() {
+  return (
+    <div>
+      <Navbar />
+      <Hero child= "HACKOTSAV"/>
+      <div className="px-5">
+        <br />
+        <About />
+        <hr />
+        <br />
+        <Schedule />
+        <hr />
+        <br />
+        <Speakers />
+        <hr />
+        <br />
+        <Sponsors />
+        <hr />
+        <br />
+        <ComPats />
+        <br />
+        <hr />
+        <br />
+        <Faq />
+        <br />
+        <hr />
+        <br />
+      </div>
+      <div />
+    </div>
+  );
+}
