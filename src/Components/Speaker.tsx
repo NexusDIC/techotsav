84cf78b04cc2f99b0a6623be6042079caf3ab749
Atharva <atharvp540@gmail.com>
2025-03-13 09:48:19 +0530

    Spekaer updated

--- src/Components/Speaker.tsx
+++ src/Components/Speaker.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { Card, CardContent } from "./ui/card";
-import Image from "next/image"
-// import {  } from "../../public/imgs/Tanmay B.png";
+import Image from "next/image";
 import {
   Carousel,
   CarouselContent,
@@ -13,10 +12,11 @@ import {
 import Autoplay from "embla-carousel-autoplay";
 
 interface Speaker {
-  name: string,
-  image: string
-  post: string,
-  Company: string
+  name: string;
+  image: string;
+  post: string;
+  Company: string;
+  linkedin: string;
 }
 
 export default function Speakers() {
@@ -25,135 +25,110 @@ export default function Speakers() {
       name: "Mr. Archit Chandak",
       image: "/imgs/Mr. Archit Chandak...png",
       post: "Deputy Commissioner of Police",
-      Company: "Nagpur City Police"
+      Company: "Nagpur City Police",
+      linkedin: "https://www.linkedin.com/in/archit-chandak-8099b9262/"
     },
     {
       name: "Mr. Nitin Sachdeva",
       image: "/imgs/Nitin s.png",
       post: "President and MD",
-      Company: "delaPlex, Nagpur"
+      Company: "delaPlex, Nagpur",
+      linkedin: "https://www.linkedin.com/in/nitin-sachdeva/"
     },
     {
       name: "Mr. Varun Gandhi",
       image: "/imgs/Varun g.jpeg",
       post: "Manager",
-      Company: "PwC Acceleration Centers, Mumbai"
+      Company: "PwC Acceleration Centers, Mumbai",
+      linkedin: "https://www.linkedin.com/in/varungandhi23/?originalSubdomain=in"
     },
     {
       name: "Mr. Aniruddha Kalbande",
       image: "/imgs/Aniruddha Kalbande.jpg",
       post: "Founder FireBlaze",
-      Company: "AISchool"
+      Company: "AISchool",
+      linkedin: "https://www.linkedin.com/in/aniruddhakalbande/?originalSubdomain=in"
     },
     {
       name: "Dr. Sachin Untawale",
       image: "/imgs/dr-sachin-untawale.jpg",
       post: "Director",
-      Company: "GHRCE"
+      Company: "GHRCE",
+      linkedin: "https://www.linkedin.com/in/sachin-untawale-17504274/?originalSubdomain=in"
     },
-    // {
-    //   name: "Ms. Sonal Shembekar",
-    //   image: "/imgs/prashant_chaudhari.jpg",
-    //   post: "Offshore Delivery Head",
-    //   Company: ""
-    // },
     {
       name: "Anmol Ranjari",
       image: "/imgs/Anmol r.jpeg",
       post: "CCS",
-      Company: ""
+      Company: "",
+      linkedin: "https://www.linkedin.com/in/amol-rangari/?originalSubdomain=in"
     },
     {
       name: "Aakash Singh",
       image: "/imgs/aakash s.jpeg",
       post: "Vice President - Product Strategy & Growth",
-      Company: ""
+      Company: "",
+      linkedin: "https://www.linkedin.com/in/aakash-singh-58479b96/?originalSubdomain=in"
     },
     {
       name: "Rahul Manusmare",
       image: "/imgs/Rahul M - Copy.jpg",
       post: "Consultant (Strategy and Analytics)",
-      Company: "Deloitte"
+      Company: "Deloitte",
+      linkedin: "https://www.linkedin.com/in/rahulmanusmare/?originalSubdomain=in"
     },
     {
       name: "Tanmay Bhat",
       image: "/imgs/Tanmay B.png",
       post: "Assistant Lead Engineer",
-      Company: "Konverge AI"
+      Company: "Konverge AI",
+      linkedin: "https://www.linkedin.com/in/tanmay-bhat-666473146/"
     },
     {
       name: "Shivam Joshi",
       image: "/imgs/shivam j.jpeg",
       post: "Founder",
-      Company: "Codons"
+      Company: "Codons",
+      linkedin: "https://www.linkedin.com/in/the-shivamjoshi/?originalSubdomain=in"
     },
     {
       name: "Priyanka Sharma",
       image: "/imgs/Priyanka s.jpeg",
       post: "Skill Trainer",
-      Company: ""
+      Company: "",
+      linkedin: "https://www.linkedin.com/in/priyanka-sharma-755265279/"
     },
     {
       name: "Nidhi Bawashe",
       image: "/imgs/Nidhi B.png",
       post: "Project Manager",
-      Company: "DelaPlex"
+      Company: "DelaPlex",
+      linkedin: "https://www.linkedin.com/in/nidhi-bawashe-7736aa10a/"
     },
-    // {
-    //   name: "Sajal Saxena",
-    //   image: "",
-    //   post: "Head of Delivery and Client Engagement",
-    //   Company: "Konverge.AI"
-    // },
     {
       name: "Rajendra Jodhpurkar",
       image: "/imgs/Rajendra J.jpeg",
       post: "CIO",
-      Company: "Infocepts"
+      Company: "Infocepts",
+      linkedin: "https://www.linkedin.com/in/raj-jodhpurkar/"
     }
   ];
 
   return (
-    <div
-      className="flex flex-col items-center justify-center gap-y-8 w-full"
-      id="speakers"
-    >
+    <div className="flex flex-col items-center justify-center gap-y-8 w-full" id="speakers">
       <h1 className="text-4xl md:text-5xl font-bold text-center">Speakers</h1>
-
-      <Carousel
-        className="w-4/5 max-w-[90dvw] sm:max-w-[80dvw] md:max-w-[70dvw] lg:max-w-[50dvw]"
-        plugins={[
-          Autoplay({
-            delay: 3000,
-          }),
-        ]}
-      >
+      <Carousel className="w-4/5 max-w-[90dvw] sm:max-w-[80dvw] md:max-w-[70dvw] lg:max-w-[50dvw]" plugins={[Autoplay({ delay: 3000 })]}>
         <CarouselContent>
           {speakers.map((speaker, index) => (
             <CarouselItem key={index} className="flex justify-center">
-              <Card className="w-full max-w-md sm:max-w-lg md:max-w-sm lg:max-w-sm  border-2 border-gray-300 shadow-xl shadow-white rounded-3xl   ">
+              <Card className="w-full max-w-md sm:max-w-lg md:max-w-sm lg:max-w-sm border-2 border-gray-300 shadow-xl shadow-white rounded-3xl">
                 <CardContent className="flex flex-col items-center justify-center p-6 space-y-2 text-center">
-                  <Image
-                    src={speaker.image}
-                    alt={speaker.name}
-                    width={250}
-                    height={250}
-                    className="w-[250px] h-[250px] object-cover rounded-lg"
-                  />
-
-                  <span className="text-xl sm:text-3xl md:text-5xl lg:text-4xl font-semibold">
-                    {speaker.name}
-                  </span>
-                  <span className="text-xl sm:text-base md:text-lg lg:text-2xl font-medium">
-                    {speaker.post}
-                  </span>
-
-                  <span className="text-base sm:text-lg md:text-xl lg:text-2xl font-medium">
-                    {speaker.Company}
-                  </span>
+                  <Image src={speaker.image} alt={speaker.name} width={250} height={250} className="w-[250px] h-[250px] object-cover rounded-lg" />
+                  <a href={speaker.linkedin} target="_blank" rel="noopener noreferrer" className="text-xl sm:text-3xl md:text-5xl lg:text-4xl font-semibold text-white-600 hover:underline">{speaker.name}</a>
+                  <span className="text-xl sm:text-base md:text-lg lg:text-2xl font-medium">{speaker.post}</span>
+                  <span className="text-base sm:text-lg md:text-xl lg:text-2xl font-medium">{speaker.Company}</span>
                 </CardContent>
               </Card>
-
             </CarouselItem>
           ))}
         </CarouselContent>
