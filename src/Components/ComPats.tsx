57d01af6272228c6b70b3c22aec9bc9d42bede4f
amitingits <amituttamdas24@gmail.com>
2025-03-15 15:47:09 +0530

    updated

--- src/Components/ComPats.tsx
+++ src/Components/ComPats.tsx
@@ -32,20 +32,16 @@ const ComPats = () => {
       image: "/imgs/coe-ml.png",
     },
     {
+      name: "Event Buzz",
+      image: "/imgs/event-buzz.jpg",
+    },
+    {
       name: "IEEE Cummins",
       image: "/imgs/ieee-cummins.jpg",
     },
     {
       name: "IEEE CS GHRCE",
       image: "/imgs/ieee-cs-logo.jpg",
-    },
-    {
-      name: "Openbox",
-      image: "/imgs/openbox-logo.png",
-    },
-    {
-      name: "Event Buzz",
-      image: "/imgs/event-buzz.jpg",
     },
     {
       name: "Nexmeet",
@@ -59,7 +55,10 @@ const ComPats = () => {
       name: "Theory 2 Practical",
       image: "/imgs/t2p-logo.png",
     },
-    
+    {
+      name: "Openbox",
+      image: "/imgs/openbox-logo.png",
+    },
   ];
 
   return (
